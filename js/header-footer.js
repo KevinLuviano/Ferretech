fc31076d48ae6d78b067a6bdcc59e3fb637e6ed1
KevinLuviano <kevinjaredluviano@gmail.com>
2026-07-17 18:37:06 -0600

    Agregar clases, estilos y js para el contador del carrito

---
+++ js/header-footer.js
@@ -0,0 +1,32 @@
+function actualizarContador() {
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    let totalArticulos = 0;
+
+    carrito.forEach(producto => {
+        totalArticulos += producto.cantidad;
+    });
+
+    const contadorElemento = document.getElementById('contador-carrito');
+    
+    if (contadorElemento) {
+        if (totalArticulos > 0) {
+            contadorElemento.innerText = totalArticulos; 
+            contadorElemento.style.display = 'block';
+        } else {
+            contadorElemento.style.display = 'none'; 
+        }
+    }
+}
+
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+        actualizarContador(); 
+    });
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    });
