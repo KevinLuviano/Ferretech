f8f01285b024a70301c8cd9dc8f53364a1b5f9d2
KevinLuviano <kevinjaredluviano@gmail.com>
2026-07-23 17:23:18 -0600

    Eliminar mensaje para la conexión exitosa

--- html/connection.php
+++ html/connection.php
@@ -9,5 +9,4 @@ $conexion = new mysqli($servidor, $usuario, $password, $base_datos);
 if ($conexion -> connect_error) {
     die("Error de conexion" . $conexion->connect_error);
 }
-echo "conexion exitosa";
 ?>
