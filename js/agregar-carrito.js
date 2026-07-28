ab116529952717f2dde0eba1f455b108e7758911
KevinLuviano <kevinjaredluviano@gmail.com>
2026-07-28 11:23:50 -0600

    Agreegar el ID al JSON para enviarlo al carrito

--- js/agregar-carrito.js
+++ js/agregar-carrito.js
@@ -8,19 +8,21 @@ for (let boton of botonesAgregar) {
         const titulo = articulo.querySelector('h3').innerText;
         const precio = articulo.querySelector('.precio').innerText;
         const imagen = articulo.querySelector('img').src;
+        const id = articulo.getAttribute('data-id');
 
         const precioNumerico = parseFloat(precio.replace(/[^0-9.]/g, ''));
 
         let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
         const productoExistente = carrito.find(function (item) {
-            return item.titulo === titulo;
+            return item.id === id;
         });
 
         if (productoExistente) {
             productoExistente.cantidad += 1;
         } else {
-            const productoSeleccionado = {
+           const productoSeleccionado = {
+                id: id,
                 titulo: titulo,
                 precio: precioNumerico,
                 imagen: imagen,
